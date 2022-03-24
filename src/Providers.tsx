9e04102a782ed88c750cdf45964d6d0416cd1224
Gabriel Wong <gabrielwonggg@gmail.com>
2022-03-24 10:13:55 +0800

    add lazy loading

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { Provider } from "react-redux";
-import Home from "./pages/Home";
-import Trade from "./pages/Trade";
-import Account from "./pages/Account";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 // import store from "./data/store";
 
+const Home = lazy(() => import("./pages/Home"));
+const Trade = lazy(() => import("./pages/Trade"));
+const Account = lazy(() => import("./pages/Account"));
+
 function Providers() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/trade" element={<Trade />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trade" element={<Trade />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
